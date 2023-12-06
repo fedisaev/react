06a6fa9ed8a2772d92302d84fadd5e506f42f660
Fed0102 <127963492+Fed0102@users.noreply.github.com>
2023-12-06 19:24:21 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [posts] = useState([
+        {id: 1, title: 'Javascript', body: 'Javascript is programming language'},
+        {id: 2, title: 'Javascript1', body: 'Javascript1 is programming language'},
+        {id: 3, title: 'Javascript2', body: 'Javascript2 is programming language'}
+    ])
+
+
+    return (
+        <div className={'App'}>
+            <form>
+                <input type="text" placeholder={'Post title'}/>
+                <input type="text" placeholder={'Post description'}/>
+                <MyButton disabled>Create post</MyButton>
+            </form>
+            <PostList posts={posts} title={'Posts list 1'}/>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/UI/input/MyInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyInput = () => {
+    return (
+        <input>
+
+        </input>
+    );
+};
+
+export default MyInput;
